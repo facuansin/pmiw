c2587abf0d170542e7d41890408036be305cf405
facuansin <129102558+facuansin@users.noreply.github.com>
2024-10-14 19:31:50 -0300

    Add files via upload

--- trabajo_final/Botones.js
+++ trabajo_final/Botones.js
@@ -1,48 +1,25 @@
-class byf {
-  constructor() {
-    this.f = 20;
-    this.drc=50;
-    this.izq=350;
-    this.down=500;
+function botonUno(x, y, an, al){
+  if(detectarBoton(x,y,an,al)){
+  fill(180);
+  }else{
+    fill(255);
   }
-
- 
-//BOTONES
-//mostrar boton 1
-mostrarBU(){
-  fill(255);  
-  rect(this.drc,this.izq,this.down,75);
-  fill(20);
-textSize(40);
-text("Siguiente",205,400);
-}
-//mostrar 2 botones
-botonDos(){
-  if(mouseX>25 && mouseX<273 && mouseY>490 && mouseY<564){
-}
-}
-mostrarDosBotones(opcion1, opcion2) {
-  fill(255);
-  rect(100, 400, 200, 60);
-  rect(350, 400, 200, 60);
-  fill(20);
-  textSize(30);
-  text(opcion1, 130, 440);
-  text(opcion2, 380, 440);
+  rect(x, y, an, al);
 }
 
-//mostrar 3 botones
-botonTres(){
-  if(dist(mouseX,mouseY,150,550)<=50){
+function botonDos(x, y, an, al){
+  if(detectarBoton2(x,y,an,al)){
+  fill(180);
+  }else{
+    fill(255);
   }
-}
-menuB(){
-  textSize(50);
-  rect(150, 200, 250, 60);
-  fill(200);
-  text("Empezar", 180, 245);
-  fill(0);
+  rect(x, y, an, al);
 }
 
 
+function detectarBoton(x, y, an, al){
+  return mouseX > x && mouseX < x+an && mouseY > y && mouseY < y+al;
+}
+function detectarBoton2(x, y, an, al){
+  return mouseX > x && mouseX < x+an && mouseY > y && mouseY < y+al;
 }
